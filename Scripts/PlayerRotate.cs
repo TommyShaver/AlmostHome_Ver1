703d7f02af70eda856508f424169ae82ebeef09e
Tommy <69636093+TommyShaver@users.noreply.github.com>
2023-03-02 16:57:15 -0800

    Added ParticleSystem
    Time to go home.

--- Scripts/PlayerRotate.cs
+++ Scripts/PlayerRotate.cs
@@ -6,10 +6,15 @@ public class PlayerRotate : MonoBehaviour
 {
     [SerializeField] float _playerRotateSpeed;
     [SerializeField] float _horizontalInput;
+
+
+    //This is how you do this. 
+    public ParticleSystem _partSystem;
     // Start is called before the first frame update
     void Start()
     {
-        
+         
+        _partSystem.Stop();
     }
 
     // Update is called once per frame
@@ -17,5 +22,9 @@ public class PlayerRotate : MonoBehaviour
     {
         _horizontalInput = Input.GetAxis("Horizontal");
         transform.Rotate(_horizontalInput * _playerRotateSpeed * Time.deltaTime * Vector3.back);
+        if(_horizontalInput == 1)
+        {
+            _partSystem.Play();
+        }
     }
 }
